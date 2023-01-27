6306b6852af63f70e7791c21dbf401f9a98a1d44
HenryG2455 <54637690+HenryG2455@users.noreply.github.com>
2023-01-27 11:50:50 -0400

    added React

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import{ useState } from 'react';
+
+async function getEmps(){
+
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Wowzer
+        </p>
+        
+      </header>
+    </div>
+  );
+}
