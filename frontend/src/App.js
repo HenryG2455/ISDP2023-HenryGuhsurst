aeb7ff178d65994a79f1bb51d0a81a6f914920dc
HenryG2455 <54637690+HenryG2455@users.noreply.github.com>
2023-01-28 00:09:49 -0400

    Added React and fetchs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import{ useState } from 'react';
 
-async function getEmps(){
 
-}
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  async function getEmps(){
+    const res = await fetch('http://localhost:8000/employee');
+    if (!res.ok) {
+      throw new Error('Data coud not be fetched!')
+    } else {
+      console.log(res.json());
+      return res;
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+      <button onClick={getEmps}> Button</button>
         <p>
           Wowzer
         </p>
